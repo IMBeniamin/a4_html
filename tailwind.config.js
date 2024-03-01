7e27b6da6023033f3a730d80f4b408c56a1d43c2
I.M. Beniamin <darkengineer@protonmail.com>
2024-03-01 15:53:05 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,vue,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        // 'print': {'raw': 'print'},
+      },
+    },
+  },
+  plugins: [],
+}
+
