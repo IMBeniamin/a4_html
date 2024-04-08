7028c8082e4a5729a54658e6ba5774d03fc83600
I.M. Beniamin <beniiorga@gmail.com>
2024-04-09 01:04:12 +0200

    implemented loading skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 import CardList from "./CardList.vue";
+import Skeleton from "primevue/skeleton";
 </script>
 
 <template>
   <div>
     <Suspense>
       <template #fallback>
-        <div>Loading...</div>
+        <div class="h-screen w-screen flex flex-row flex-wrap justify-around items-center
+        bg-white dark:bg-slate-900">
+          <template v-for="_ in 20">
+            <Skeleton width="10rem" height="12rem" borderRadius="1rem"></Skeleton>
+          </template>
+        </div>
       </template>
-      <CardList />
+      <template #default>
+        <CardList />
+      </template>
     </Suspense>
   </div>
 </template>
