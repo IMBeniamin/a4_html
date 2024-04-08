9d982a4a39de4cce87f716e9f1947ae28a5f05b6
I.M. Beniamin <beniiorga@gmail.com>
2024-04-09 00:06:36 +0200

    added vue tailwindcss presets

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
+import Lara from './presets/lara/index.js'
 
 const app = createApp(App)
 app.use(PrimeVue, {
     ripple: true,
-    unstyled: true
+    unstyled: true,
+    pt: Lara
 });
 app.mount('#app')
