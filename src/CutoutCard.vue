f8156ce102f4befb421c6602b4a7c593fd42e40b
I.M. Beniamin <beniiorga@gmail.com>
2024-04-09 00:49:56 +0200

    implemented dark mode

--- src/CutoutCard.vue
+++ src/CutoutCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import {ref, watch} from 'vue';
+import InputNumber from "primevue/inputnumber";
 
 const props = defineProps({
   'title': String,
@@ -8,7 +9,7 @@ const props = defineProps({
 const emit = defineEmits(["edit-count"]);
 const title = ref(props.title)
 const count = ref(props.count)
-watch(count, () => emit('edit-count', { title: title.value, count: count.value }))
+watch(count, () => emit('edit-count', {title: title.value, count: count.value}))
 
 </script>
 
@@ -18,28 +19,34 @@ watch(count, () => emit('edit-count', { title: title.value, count: count.value }
               flex flex-col justify-start
               transition-colors duration-100 ease-in-out
               bg-slate-100
+              dark:bg-slate-800
               hover:bg-slate-200
+              dark:hover:bg-slate-700
               checked:bg-green-200
+              dark:checked:bg-green-300
+              dark:text-slate-100
               ">
-    <div id="high-padding" class="basis-4/5 border-x-slate-50 border-b-2 ">
-    </div>
-    <div id="low-padding" class="basis-1/5">
-
-    </div>
+    <div id="high-padding" class="basis-4/5
+    border-x-slate-50 dark:border-x-slate-700
+    border-b-2 dark:border-b-2
+    border-slate-200 dark:border-slate-600"/>
+    <div id="low-padding" class="basis-1/5"/>
     <div class="font-bold text-3xl leading-none
                 flex flex-col align-middle
                 ">
       <p class="text-center">{{ title }}</p>
-      <input class="text-center bg-inherit" type="number" required v-model="count" />
+      <input class="text-center bg-inherit" type="number" required v-model="count"/>
     </div>
   </div>
 </template>
 
 <style scoped>
 @tailwind components;
+
 @layer components {
   .card:has(input:checked) {
     @apply bg-green-200;
+    @apply dark:bg-green-300;
   }
 }
 
